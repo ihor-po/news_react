85edba7e632458a2ce01d3c1c29dbfd500c6a7cc
Ihor Porotikov <sobikashi@gmail.com>
2019-02-06 10:57:54 +0100

    incriment

--- src/modules/comments/commentsComponent.js
+++ src/modules/comments/commentsComponent.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
 class CommentsComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.comments = this.props.comments;
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState({comments: ++this.comments});
+    }  
   render() {
     return (
-      <div className="comments">
-        Коментариев 0
+      <div className="comments" onClick={this.handleClick}>
+        Коментариев {this.comments}
       </div>
     );
   }
--- src/modules/news/singleNewsCompoent.js
+++ src/modules/news/singleNewsCompoent.js
@@ -18,24 +18,23 @@ class SingleNewsComponent extends Component {
     let _text = this.props.previewText;
     let _fullText = this.props.fullText;
     let _visible = this.state.visible;
+
     return (  
         <div className="news__singleNews">
           <h3 className="news__title"><i className="material-icons">fiber_new</i>{_title}</h3>
           <p className="news__text">{_text}</p>
           <p className={"news__full-text " + (_visible ? "" : "d-none")}>{_fullText}</p>
-          {/* <button className={" " + (_visible ? "d-none" : "mdc-button")} onClick={this.handleClick}>
-            <span className={(_visible ? "d-none" : "mdc-button__label")}>Read</span>
-          </button> */}
-          <button id="star-this-item"
-   className={(_visible ? "d-none" : "mdc-icon-button mdc-icon-button--on")}
-   aria-label="Unstar this item"
-   aria-hidden="true"
-   aria-pressed="true"
-   onClick={this.handleClick}>
-   <img src="" className="mdc-icon-button__icon"/><i class="material-icons">fingerprint</i>
-   {/* <img src="" className="mdc-icon-button__icon"/><i class="material-icons">details</i> */}
-</button>
-          <CommentsComponent/>
+          <button 
+            id="star-this-item"
+            className={(_visible ? "d-none" : "mdc-icon-button mdc-icon-button--on")}
+            aria-label="Unstar this item"
+            aria-hidden="true"
+            aria-pressed="true"
+            onClick={this.handleClick}
+          >
+              <img src="" className="mdc-icon-button__icon" alt=""/><i className="material-icons">fingerprint</i>
+          </button>
+          <CommentsComponent comments={1}/>
         </div>
     );
   }
